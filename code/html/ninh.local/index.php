a6d7408f38049ac6baa6941f5e278eab84d227bc
tudv <tudv@hachinet.vn>
2020-12-22 14:51:13 +0700

    auto compile reactjs

--- code/html/ninh.local/index.php
+++ code/html/ninh.local/index.php
@@ -1,19 +1,3 @@
-<?php 
+<?php
 
-
-function checkdata($number){
-	if($number > 10){
-    	throw new Exception("Number is greater than 10");
-    }
-    return true;
-}
-checkdata(15);
-// try block starts
-// try{
-// 	checkdata(15);
-//   	echo "The number is below 10";
-// }
-// // catch block
-// catch(Exception $e){
-// 	echo "Message :".$e->getMessage();
-// }
+phpinfo();
