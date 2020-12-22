4b621e1a7aef5765b4f6690d688b113f21f70e36
tudv <tudv@hachinet.vn>
2020-12-22 09:35:36 +0700

    fix

--- code/html/ninh.local/index.php
+++ code/html/ninh.local/index.php
@@ -1,4 +1,19 @@
 <?php 
 
 
-echo phpinfo();
+function checkdata($number){
+	if($number > 10){
+    	throw new Exception("Number is greater than 10");
+    }
+    return true;
+}
+checkdata(15);
+// try block starts
+// try{
+// 	checkdata(15);
+//   	echo "The number is below 10";
+// }
+// // catch block
+// catch(Exception $e){
+// 	echo "Message :".$e->getMessage();
+// }
